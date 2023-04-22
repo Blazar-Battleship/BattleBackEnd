4fc20709a668cfc5acf139fa36125bb5b54c43bb
MIGUEL ASTOPILLO <condoria.miguel@gmail.com>
2023-04-22 23:28:10 +0200

    Completed Player Task

--- Battleship/Battleship/Controllers/CoalitionsController.cs
+++ Battleship/Battleship/Controllers/CoalitionsController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Battleship.Models;
+using System.Numerics;
+using Newtonsoft.Json.Linq;
 
 namespace Battleship.Controllers
 {
@@ -21,15 +23,15 @@ namespace Battleship.Controllers
         }
 
         // GET: api/Coalitions
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Coalition>>> GetCoalitions()
-        {
-          if (_context.Coalitions == null)
-          {
-              return NotFound();
-          }
-            return await _context.Coalitions.ToListAsync();
-        }
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<Coalition>>> GetCoalitions()
+        //{
+        //  if (_context.Coalitions == null)
+        //  {
+        //      return NotFound();
+        //  }
+        //    return await _context.Coalitions.ToListAsync();
+        //}
 
         // GET: api/Coalitions/5
         [HttpGet("{id}")]
@@ -83,16 +85,57 @@ namespace Battleship.Controllers
         // POST: api/Coalitions
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Coalition>> PostCoalition(Coalition coalition)
+        public async Task<ActionResult<Game>> PostCoalition(Player[] players)
         {
-          if (_context.Coalitions == null)
-          {
-              return Problem("Entity set 'BattleShipContext.Coalitions'  is null.");
-          }
-            _context.Coalitions.Add(coalition);
+            if (_context.Coalitions == null)
+            {
+                return Problem("Entity set 'BattleShipContext.Coalitions'  is null.");
+            }
+
+            Game game = new Game();
+            Coalition coal1 = new Coalition();
+            Coalition coal2 = new Coalition();
+
+            game.Coalitions.Add(coal1);
+            game.Coalitions.Add(coal2);
+
+            if(players.Length == 2)
+            {
+                Player player1 = players[0];
+                Player player2 = players[1];
+
+                coal1.Players.Add(player1);
+                coal2.Players.Add(player2);
+            }else if(players.Length > 2)
+            {
+                var rnd = new Random();
+                List<int> numbers = Enumerable.Range(0, players.Length).OrderBy(x => rnd.Next()).Take(players.Length).ToList();
+                int num = 0;
+
+                Random rand = new Random();
+                Console.WriteLine(numbers);
+                foreach (Player i in players.OrderBy(x => rand.Next()))
+                {
+                    if(num == 0)
+                    {
+                        Player play1 = i;
+                        coal1.Players.Add(play1);
+                        num = 1;
+                    }
+                    else
+                    {
+                        Player play2 = i;
+                        coal2.Players.Add(play2);
+                        num = 0;
+                    }           
+
+                }
+            }
+
+            _context.Games.Add(game);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetCoalition", new { id = coalition.Id }, coalition);
+            return game;
         }
 
         // DELETE: api/Coalitions/5
